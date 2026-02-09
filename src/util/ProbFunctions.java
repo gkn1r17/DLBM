6698a87f217c13c470e82948cc64bec8580f1d42
gkn1r17 <135731749+gkn1r17@users.noreply.github.com>
2026-02-09 07:05:24 +0000

    Refactoring
    rearranging packages

--- src/util/ProbFunctions.java
+++ src/util/ProbFunctions.java
@@ -5,19 +5,20 @@ import cern.jet.random.engine.DRand;
 
 public class ProbFunctions {
 	 
-		public static int getBinomial(int num, double prob, Binomial bn, DRand rd) throws Exception {
-			if(num == 0)
+
+		public static int getBinomial(int n, double probability, Binomial bn, DRand rd) throws Exception {
+			if(n == 0)
 				return 0;
-			else if(num < 0) {
-				throw new Exception("Error in getBinomial, num = " + num);
+			else if(n < 0) {
+				throw new Exception("Error in getBinomial, num = " + n);
 			}
-			if(Math.ceil(0.0099 / (num*prob)) > 1) { //if probability too low to calculate binomial (just limitation of library)
+			if(Math.ceil(0.0099 / (n*probability)) > 1) { //if probability too low to calculate binomial (just limitation of library)
 				if(rd == null) //either:
 					return -1; //return error flag
 				else { //make num separate uniform probability decisions with probability prob (functionally same as binomial)
 						int outVal = 0;
-						for(int i =0; i < num; i++) {
-							if(rd.nextDouble() < prob)
+						for(int i =0; i < n; i++) {
+							if(rd.nextDouble() < probability)
 								outVal++;
 						}
 						return outVal;
@@ -26,9 +27,9 @@ public class ProbFunctions {
 			}
 			else { //if not return binomial
 				try {
-					return bn.nextInt(num, prob);
+					return bn.nextInt(n, probability);
 				}catch(java.lang.IllegalArgumentException e) {
-					throw(new Exception("Error in getBinomial: num = " + num + " prob = " + prob));
+					throw(new Exception("Error in getBinomial: num = " + n + " prob = " + probability));
 				}
 			}
 
