8bd8ecbae7e8689ac0db68495dcc2caca75ed2fd
gkn1r17 <135731749+gkn1r17@users.noreply.github.com>
2026-05-01 14:41:01 +0100

    Add files via upload

---
+++ run/runLBM.local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#add/remove based on necessary boiler plate code for using java in your system. Must be at least jdk1.8
+module load jdk
+#
+
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 LBM.jar [PROGRAM_ARGS]"
+    exit 1
+fi
+
+PROGRAM=$1
+shift 1
+
+JAVA_BIN=${JAVA_BIN:-java}
+JAVA_OPTS=${JAVA_OPTS:-"-Xmx32g -Xms4g"}
+
+exec "$JAVA_BIN" $JAVA_OPTS -jar "$PROGRAM" "$@" 
